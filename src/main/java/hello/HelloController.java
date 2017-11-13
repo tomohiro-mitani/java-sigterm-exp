e10660ad08ccd52d8dfdfa42290fadcde60d34a3
Tomohiro Mitani <tmitani@tmitani-ltm2.internal.salesforce.com>
2017-11-13 18:02:26 +0900

    more responses

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,12 +8,37 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/5")
+    public String index2() {
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             return "Interruption Exception";
         }
-        return "Greetings from Spring Boot!";
+        return "5 Seconds Response!!!!!!!!!!";
     }
-    
+
+    @RequestMapping("/10")
+    public String index3() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            return "Interruption Exception";
+        }
+        return "5 Seconds Response!!!!!!!!!!";
+    }
+
+    @RequestMapping("/30")
+    public String index4() {
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            return "Interruption Exception";
+        }
+        return "5 Seconds Response!!!!!!!!!!";
+    }
+
 }
